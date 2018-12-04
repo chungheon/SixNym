e9e3dbaa399f73959eca64785a6a3f4088ace31b
Tony <looty89@gmail.com>
2018-12-04 23:57:29 +0800

    added permissions to manifest and start server control

---
+++ app/src/main/java/com/looting/sixnym/GameServerController.java
@@ -0,0 +1,12 @@
+package com.looting.sixnym;
+
+import android.net.wifi.p2p.WifiP2pManager;
+
+public class GameServerController {
+    int numOfPlayers = 1;
+    public GameServerController(int numOfPlayers) {
+        this.numOfPlayers = numOfPlayers;
+    }
+
+
+}
